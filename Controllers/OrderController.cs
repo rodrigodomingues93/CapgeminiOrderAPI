97036e0fc974a09ed105273be53a7b00c73f5640
Rodrigo Domingues <rodrigo.domingues.93@gmail.com>
2022-03-31 15:55:40 +0100

    Add project files.

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,56 @@
+﻿using CapgeminiOrderAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapgeminiOrderAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class OrderController : ControllerBase
+	{
+		private static List<OrderModel> Orders = new List<OrderModel>();
+
+		[HttpPost]
+		public IActionResult Post([FromBody] OrderModel order)
+		{
+			if (order.Id == System.Guid.Empty)
+				return BadRequest();
+
+			Orders.Add(order);
+			return Ok(order);
+		}
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok(Orders);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult Get(Guid id)
+		{
+			return Ok(Orders.FirstOrDefault(w => w.Id == id));
+		}
+
+		[HttpPut]
+		public IActionResult Put([FromBody] OrderModel order)
+		{
+			var orderUpdate= Orders.FirstOrDefault(w => w.Id == order.Id);
+			if (orderUpdate == null)
+				return NotFound();
+
+			orderUpdate.Number= order.Number;
+			orderUpdate.Client= order.Client;
+			orderUpdate.Adress= order.Adress;
+			orderUpdate.Product= order.Product;
+			return Ok(order);
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(Guid id)
+		{
+			var orderDelete= Orders.FirstOrDefault(w => w.Id ==id);
+			Orders.Remove(orderDelete);
+			return Ok(true);
+		}
+	}
+}
---
+++ Models/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CapgeminiOrderAPI.Models
+{
+	public class OrderModel
+	{
+		public Guid Id { get; set; }
+		public string Number { get; set; }
+		public string Client { get; set; }
+		public string Adress { get; set; }
+		public string Product { get; set; }
+	}
+}
